a611e3e0ded96ed5b538dce392b5764ef4e5447f
chaimaeasrissar <chaimaeasrissar@gmail.com>
2024-01-14 17:30:15 -0800

    a

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,13 +3,15 @@ from fabric.api import local
 from time import strftime
 from datetime import date
 
+
 def do_pack():
     """ A script that generates archive the contents of web_static folder"""
 
     filename = strftime("%Y%m%d%H%M%S")
     try:
         local("mkdir -p versions")
-        local("tar -czvf versions/web_static_{}.tgz web_static/".format(filename))
+        local("tar -czvf versions/web_static_{}.tgz web_static/"
+              .format(filename))
 
         return "versions/web_static_{}.tgz".format(filename)
 
--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -5,25 +5,25 @@ from fabric.api import *
 env.hosts = ['100.25.118.26', '3.86.7.205']
 
 
-        def do_clean(number=0):
-                """Delete out-of-date archives.
+def do_clean(number=0):
+    """Delete out-of-date archives.
 
-                    Args:
-                            number (int): The number of archives to keep.
+    Args:
+        number (int): The number of archives to keep.
 
-                                If number is 0 or 1, keeps only the most recent archive. If
-                                    number is 2, keeps the most and second-most recent archives,
-                                        etc.
-                                            """
-                                                number = 1 if int(number) == 0 else int(number)
+    If number is 0 or 1, keeps only the most recent archive. If
+    number is 2, keeps the most and second-most recent archives,
+    etc.
+    """
+    number = 1 if int(number) == 0 else int(number)
 
-                                                    archives = sorted(os.listdir("versions"))
-                                                        [archives.pop() for i in range(number)]
-                                                            with lcd("versions"):
-                                                                        [local("rm ./{}".format(a)) for a in archives]
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in archives]
 
-                                                                            with cd("/data/web_static/releases"):
-                                                                                        archives = run("ls -tr").split()
-                                                                                                archives = [a for a in archives if "web_static_" in a]
-                                                                                                        [archives.pop() for i in range(number)]
-                                                                                                                [run("rm -rf ./{}".format(a)) for a in archives]
+    with cd("/data/web_static/releases"):
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]
