6557a9b58ecba245049d1025fbd69e4a5df4861a
chaimaeasrissar <chaimaeasrissar@gmail.com>
2024-01-28 08:35:57 -0800

    d

--- models/state.py
+++ models/state.py
@@ -1,35 +1,36 @@
 #!/usr/bin/python3
-"""Defines the State class."""
-import models
-from os import getenv
-from models.base_model import Base
-from models.base_model import BaseModel
-from models.city import City
-from sqlalchemy import Column
-from sqlalchemy import String
+""" State Module for HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
+from models.city import City
+from os import getenv
+
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
 class State(BaseModel, Base):
-    """Represents a state for a MySQL database.
+    """State class"""
 
-    Inherits from SQLAlchemy Base and links to the MySQL table states.
-
-    Attributes:
-        __tablename__ (str): The name of the MySQL table to store States.
-        name (sqlalchemy String): The name of the State.
-        cities (sqlalchemy relationship): The State-City relationship.
-    """
     __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City",  backref="state", cascade="delete")
+    if storage_type == "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state", cascade="all,delete")
+    else:
+        name = ""
 
-    if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
-            """Get a list of all related City objects."""
-            city_list = []
-            for city in models.storage.all(City).values():
+            """
+            getter attribute cities that returns the list of City instances
+            with state_id equals to the current State.id => It will
+            be the FileStorage relationship between State and City
+            """
+            from models import storage
+
+            citiesList = []
+            citiesAll = storage.all(City)
+            for city in citiesAll.values():
                 if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+                    citiesList.append(city)
+            return citiesList
